7c0cedbdc85f12fcfdda36c9618ffe34f91aff4a
Carlos Rios <intercrios@hotmail.com>
2022-03-11 23:46:36 -0300

    Add files via upload
    07 - Springboot-thymeleaf   html css js

---
+++ 07 - springboot-thymeleaf/src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController{
+
+	@GetMapping("/main")
+	public String greeting(@RequestParam(name="nombre", required=false, defaultValue="World") String nombre, Model model){
+		model.addAttribute("nombre",nombre);
+		return"main";
+	}
+}
